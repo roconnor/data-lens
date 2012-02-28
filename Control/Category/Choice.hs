68ff1fd1eb8276b783d93df7c304f54aa18711b1
Tony Morris <tmorris@tmorris.net>
2012-02-28 14:02:04 +1000

    choice, codiagonal and product instances for lens and partial lens

--- Control/Category/Choice.hs
+++ Control/Category/Choice.hs
@@ -1,5 +1,22 @@
 module Control.Category.Choice where
 
+import Data.Lens.Common
+import Data.Lens.Partial.Common
+import Control.Comonad.Trans.Store
+
+infixr 2 |||
 class Choice c where
   (|||) :: c x z -> c y z -> c (Either x y) z
-  
+
+instance Choice Lens where
+  Lens f ||| Lens g =
+    Lens $ either
+      (\a -> let x = f a in store (Left . flip peek x) (pos x))
+      (\b -> let y = g b in store (Right . flip peek y) (pos y))
+
+instance Choice PartialLens where
+  PLens f ||| PLens g =
+    PLens $ either
+      (\a -> fmap (\x -> store (Left . flip peek x) (pos x)) (f a))
+      (\b -> fmap (\y -> store (Right . flip peek y) (pos y)) (g b))
+
--- Control/Category/Product.hs
+++ Control/Category/Product.hs
@@ -1,5 +1,24 @@
 module Control.Category.Product where
 
+import Data.Lens.Common
+import Data.Lens.Partial.Common
+import Control.Comonad.Trans.Store
+
+infixr 3 ***
 class Product c where
   (***) :: c w x -> c y z -> c (w, y) (x, z)
   
+instance Product Lens where
+  Lens f *** Lens g =
+    Lens $ \(a, c) ->
+      let x = f a
+          y = g c
+      in store (\(b, d) -> (peek b x, peek d y)) (pos x, pos y)
+
+instance Product PartialLens where
+  PLens f *** PLens g =
+    PLens $ \(a, c) ->
+      do x <- f a
+         y <- g c
+         return $ store (\(b, d) -> (peek b x, peek d y)) (pos x, pos y)
+
