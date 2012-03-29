b059a75f39597accc076a7767f7ddc27b58af4f7
Tony Morris <tmorris@tmorris.net>
2012-03-30 08:27:25 +1000

    instances for Choice, Codiagonal, Split

--- src/Control/Category/Choice.hs
+++ src/Control/Category/Choice.hs
@@ -1,5 +1,13 @@
 module Control.Category.Choice where
 
+import Control.Arrow
+
 infixr 2 |||
 class Choice c where
   (|||) :: c x z -> c y z -> c (Either x y) z
+
+instance Choice (->) where
+  (|||) = either
+
+instance Choice (Kleisli m) where
+  Kleisli f ||| Kleisli g = Kleisli $ either f g
--- src/Control/Category/Split.hs
+++ src/Control/Category/Split.hs
@@ -1,6 +1,16 @@
 module Control.Category.Split where
 
+import Control.Arrow
+import Control.Applicative
+
 infixr 3 ***
 class Split c where
   (***) :: c w x -> c y z -> c (w, y) (x, z)
 
+instance Split (->) where
+  (***) f g (w, y) =
+    (f w, g y)
+
+instance Applicative m => Split (Kleisli m) where
+  Kleisli f *** Kleisli g =
+    Kleisli $ \(w, y) -> liftA2 (,) (f w) (g y)
