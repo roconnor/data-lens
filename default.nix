c68a1613e102d8d0ccd5bb7c1e23b36a9581fe87
Russell O'Connor <oconnorr@google.com>
2015-05-31 14:37:48 -0400

    Bump dependency on semigroupoids

--- default.nix
+++ default.nix
@@ -1,11 +1,10 @@
 { pkgs ? import <nixpkgs> {}
 , hp ? pkgs.haskellPackages
-, src ? ./.
 }:
 
 hp.cabal.mkDerivation (self: {
   pname = "data-lens";
-  version = "2.10.5";
-  inherit src;
-  buildDepends = [ hp.comonad hp.semigroupoids hp.transformers ];
+  version = "2.10.6";
+  src = pkgs.lib.sourceFilesBySuffices ./. [".hs" ".cabal" "CHANGELOG" "LICENSE"];
+  buildDepends = [ hp.comonad hp.semigroupoids ];
 })
