edd8b249015374cab11bd7890dc9cf1c6e8b41da
Russell O'Connor <roconnor@theorem.ca>
2017-12-31 23:20:10 -0500

    Bump dependencies on semigroupoids.

--- default.nix
+++ default.nix
@@ -6,7 +6,7 @@ hp.callPackage
  ({ mkDerivation, comonad, semigroupoids }:
   mkDerivation {
    pname = "data-lens";
-   version = "2.11.1";
+   version = "2.11.2";
    src = pkgs.lib.sourceFilesBySuffices ./. [".hs" ".cabal" "CHANGELOG" "LICENSE"];
    buildDepends = [ comonad semigroupoids ];
    license = pkgs.lib.licenses.bsd3;
